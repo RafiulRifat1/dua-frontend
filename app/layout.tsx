38bd1faf60e81f78ce7f3d6c944cc3c27b4b4b8d
MD Rafiul Islam Rifat <rafiul2071@gmail.com>
2025-11-14 00:07:56 +0600

    full nextjs setup with type and tailwind also added SIDEBARS

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import RightSidebar from "./RightSidebar";
+import Sidebar from "./Sidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Sidebar />
+        <main className="min-h-screen bg-black text-white md:mr-[349px] md:ml-64">
+          {children}
+        </main>
+        <RightSidebar />
       </body>
     </html>
   );
